af80c2402b56a1ab5fd0fe069ff6a8b468bf7366
Guilherme Breve <foxtec198@gmail.com>
2024-02-23 11:24:33 -0300

    .

--- authWA.py
+++ authWA.py
@@ -10,11 +10,19 @@ from datetime import datetime #Tempo Real
 import smtplib # Envio de email
 import email.message # Montagemd do Email
 from win32com import client
+from webbrowser import open_new_tab
 
+pg.PAUSE = 1
+pg.FAILSAFE = False
 
 def atalho(*args):
     with pg.hold(args[0]):
         pg.press(args[1])
+
+def atalho2(*args):
+    with pg.hold(args[0]):
+        pg.press(args[1])
+        pg.press(args[3])
 
 def cola(txt: str):
     copy(txt)
@@ -23,23 +31,23 @@ def cola(txt: str):
 def display(x):
     print(st(f'%X - Horario de inicio {x} '))
     sl(1)
-    system('cls')
 
 def conectar_email(em, pwd):
     global sm, emailFrom
     emailFrom = em
-    sm = smtplib.SMTP('smtp-gmail.com', 587)
-    sm.login(emailFrom, pwd)
+    sm = smtplib.SMTP('smtp.gmail.com', 587)
     sm.starttls()
     try:
+        sm.login(emailFrom, pwd)
         ...
     except Exception as e:
         print(e)
 
 def enviar_email(erro, data):
-    corpo_email = """
-
-    """,
+    corpo_email = f"""
+    <p>{erro}</p>
+    <p>{data}</p>
+    """
     
     msg = email.message.Message()
     msg['Subject'] = "Erro no AuthWA" # Assunto 
@@ -51,8 +59,6 @@ def enviar_email(erro, data):
     sm.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
     print('Erro enviado por email, ja iremos tratar o problema ☺')
     sl(3)
-
-conectar_email('foxtec198@gmail.com','vewmksduxchpjirg')
 
 class WA:
     def __init__(self):
@@ -98,6 +104,29 @@ class WA:
         pg.press('backspace')
         # atalho('alt','tab')
 
+    def enviar_msg_web(self, nome, mensagem, img = None):
+        # Pesquisa a Conversa
+        atalho2('ctrl','alt','k')
+        sl(2)
+        cola(nome)
+        sl(2)
+        # Entra na conversa
+        pg.press('enter')
+        sl(5)
+
+        if img: # Caso tenha Imagem
+            self.pc.write_image_to_clipboard(img)
+            atalho('ctrl','v')
+            sl(5)
+            cola(mensagem)
+            sl(1)
+            pg.press('enter')
+            pg.press('esc')
+        else: # Caso não tenha Imagem
+            cola(mensagem)
+            pg.press('enter')
+            pg.press('esc')
+
     def criar_imagem_SQL(self, consulta, arquivo = 'dist/temp.png'):
         with open('dist/temp.png', 'w') as f:
             f.close()
@@ -111,7 +140,7 @@ class WA:
 
 class Parcial:
     def __init__(self, uid, pwd, server, 
-                 hora_inicio = 8, hora_final = 18,):
+                 hora_inicio = 0, hora_final = 23,):
 
         self.hora_inicio = hora_inicio
         self.hora_final = hora_final
@@ -127,6 +156,7 @@ class Parcial:
         self.day = st('%d')
         self.month = st('%m')
         self.year = st('%Y')
+        self.fds = st('%a')
 
     def definir_inicio(self):
         self.update()
@@ -134,8 +164,8 @@ class Parcial:
         
         if self.hora == 0: alternated = 0
         if self.hora_inicio == self.hora: alternated = self.hora_inicio
-        if self.hora > self.hora_inicio: alternated = self.hora + 1
         if self.hora < self.hora_inicio: alternated = self.hora_inicio
+        if self.hora > self.hora_inicio: alternated = self.hora + 1
 
         return alternated
     
@@ -143,11 +173,9 @@ class Parcial:
         h = self.definir_inicio()
         if type(funcs) == list: 
             while True:
-                fds = st('%a')
                 self.update()
                 if h == 24: h = 0
                 for f in funcs:
-                    if self.hora > self.hora_final: h = self.hora_inicio
                     if type(f) == dict:
                         sl(1)
                         he = st('%X')
@@ -159,25 +187,26 @@ class Parcial:
                                     atalho('alt','tab')
                                     func()
                                     atalho('alt','tab')
-                                except Exception as erro: enviar_email(erro, st('%x - %X'))
+                                except Exception as erro: 
+                                    enviar_email(erro, st('%x - %X'))
                     if self.hora == h:
-                        atalho('alt','tab')
-                        sl(1)
-                        if type(f) == list and fds == 'Sat' or fds == 'Sun':
+                        if type(f) == list and self.fds == 'Sat' or self.fds == 'Sun':
                             try:
+                                atalho('alt','tab')
                                 for i in f:
                                     i()
                                 h += 1
+                                atalho('alt','tab')
                             except Exception as erro: enviar_email(erro, st('%x - %X'))
-                        if type(f) == tuple and fds != 'Sat' and fds != 'Sun':
+                        if type(f) == tuple and self.fds != 'Sat' and self.fds != 'Sun':
                             try:
+                                atalho('alt','tab')
                                 for i in f:
                                     i()
                                 h += 1
+                                atalho('alt','tab')
                             except Exception as erro: enviar_email(erro, st('%x - %X'))
-                        atalho('alt','tab')
                     display(h)
         else: return 'Isto não é uma lista'
 
-if __name__ == '__main__':
-    ...
+conectar_email('foxtec198@gmail.com','vewmksduxchpjirg')
