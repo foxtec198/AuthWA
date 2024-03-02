075d44546781edb51b6209d8c32ee3b2394e7be9
Guilherme Breve <foxtec198@gmail.com>
2024-03-02 13:20:21 -0300

    .

--- authWA.py
+++ authWA.py
@@ -4,16 +4,19 @@ from pyperclip import copy # Trad Clipboard
 from ctpaperclip import PyClipboardPlus # Image to Clipboard
 from pandas import read_sql # Tratamento de Dados
 from dataframe_image import export # Export png
-from os import system, mkdir, startfile # Systems
+from os import system, mkdir # Systems
 from sqlalchemy import create_engine # SQL Server
 from datetime import datetime #Tempo Real
 import smtplib # Envio de email
 import email.message # Montagemd do Email
-from win32com import client
-from webbrowser import open_new_tab
+import logging # Logs
 
 pg.PAUSE = 1
 pg.FAILSAFE = False
+try: mkdir('logs')
+except: ...
+logging.basicConfig(filename='logs/error_logs.log', filemode='a', level=logging.WARNING, format="Horario do erro: %(asctime)s - %(levelname)s, Aqruivo: %(filename)s, Mensagem:%(message)s")
+logger = logging.getLogger('root')
 
 def atalho(*args):
     with pg.hold(args[0]):
@@ -31,6 +34,7 @@ def cola(txt: str):
 def display(x):
     print(st(f'%X - Horario de inicio {x} '))
     sl(1)
+    system('cls')
 
 def conectar_email(em, pwd):
     global sm, emailFrom
@@ -43,10 +47,29 @@ def conectar_email(em, pwd):
     except Exception as e:
         print(e)
 
-def enviar_email(erro, data):
+def enviar_erro(erro, data):
     corpo_email = f"""
-    <p>{erro}</p>
-    <p>{data}</p>
+    <body style="background-color: black; color: white; font-family: Arial, Helvetica, sans-serif">
+        <div style="display: flex; justify-content: center; justify-items: center;align-items: center;">
+            <div>
+                <img src='https://portalprod.gpsvista.com.br/assets/media/logos/Gpslogo.svg' style="width: 200px;"/>
+            </div>
+        </div>
+        <div style="margin-left: 30px;">
+            <h1>Log de Erros</h1>
+            <p>O <b> Parcial(AuthWA) </b> obteve um erro na data {data}</p>
+            <p>{erro}</p>
+        </div>
+        <div style="margin-top: 300px; margin-left: 50px; margin-bottom: 50px; display: flex;">
+            <img src="https://avatars.githubusercontent.com/u/64221923?v=4" alt="foto" style="border-radius: 50%; width: 200px;"/>
+            <dib style="font-family: 'Courier New', Courier, monospace; margin-left: 30px; margin-top: 30px;">
+                <p style="margin-bottom: -10;">Att</p>
+                <p style="margin-bottom: -10;">Guilherme Breve</p>
+                <p style="margin-bottom: -10;">Analista de Projetos</p>
+                <p style="color: gray; margin-bottom: -10;"><i>+55 439966617904</i></p>
+            </dib>
+        </div>
+    </body>
     """
     
     msg = email.message.Message()
@@ -59,6 +82,13 @@ def enviar_email(erro, data):
     sm.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
     print('Erro enviado por email, ja iremos tratar o problema ☺')
     sl(3)
+
+def enviar_email(erro, data):
+    try:
+        enviar_erro(erro, data)
+        logger.error(erro)
+    except:
+        logger.error(erro)
 
 class WA:
     def __init__(self):
@@ -128,7 +158,7 @@ class WA:
             pg.press('esc')
 
     def criar_imagem_SQL(self, consulta, arquivo = 'dist/temp.png'):
-        with open('dist/temp.png', 'w') as f:
+        with open(arquivo, 'w') as f:
             f.close()
         try:
             df = read_sql(consulta, self.conn)
@@ -137,6 +167,7 @@ class WA:
         except Exception as erro:
             enviar_email(erro, st('%x - %X'))
             return arquivo
+        sl(3)
 
 class Parcial:
     def __init__(self, uid, pwd, server, 
@@ -171,13 +202,13 @@ class Parcial:
     
     def main_loop(self, funcs: list):
         h = self.definir_inicio()
+        print(st(f'%X - Horario de inicio {h} '))
         if type(funcs) == list: 
             while True:
                 self.update()
                 if h == 24: h = 0
                 for f in funcs:
                     if type(f) == dict:
-                        sl(1)
                         he = st('%X')
                         for item in f:
                             horario = item
@@ -188,7 +219,8 @@ class Parcial:
                                     func()
                                     atalho('alt','tab')
                                 except Exception as erro: 
-                                    enviar_email(erro, st('%x - %X'))
+                                    enviar_email(str(erro), st('%x - %X'))
+                                    continue
                     if self.hora == h:
                         if type(f) == list and self.fds == 'Sat' or self.fds == 'Sun':
                             try:
@@ -197,7 +229,9 @@ class Parcial:
                                     i()
                                 h += 1
                                 atalho('alt','tab')
-                            except Exception as erro: enviar_email(erro, st('%x - %X'))
+                            except Exception as erro:
+                                enviar_email(str(erro), st('%x - %X'))
+                                continue
                         if type(f) == tuple and self.fds != 'Sat' and self.fds != 'Sun':
                             try:
                                 atalho('alt','tab')
@@ -205,7 +239,9 @@ class Parcial:
                                     i()
                                 h += 1
                                 atalho('alt','tab')
-                            except Exception as erro: enviar_email(erro, st('%x - %X'))
+                            except Exception as erro: 
+                                enviar_email(str(erro), st('%x - %X'))
+                                continue
                     display(h)
         else: return 'Isto não é uma lista'
 
